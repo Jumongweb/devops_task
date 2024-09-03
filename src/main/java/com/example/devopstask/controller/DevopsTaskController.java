9acbdc49af9170e716053bc46f0257cfdc2899e2
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-09-03 10:16:35 +0100

    dev

--- src/main/java/com/example/devopstask/controller/DevopsTaskController.java
+++ src/main/java/com/example/devopstask/controller/DevopsTaskController.java
@@ -1,2 +1,14 @@
-package com.example.devopstask.controller;public class DevopsTaskController {
+package com.example.devopstask.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevopsTaskController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello, i am done with my task!";
+    }
+
 }
